6824788111c1026de78a9fbd87dfb0b785b73b5b
ApMatheus <matheusaparecido101@gmail.com>
2024-06-22 19:11:04 -0300

    feat: CategoryList

---
+++ loaders/Blog/CategoryList.tsx
@@ -0,0 +1,20 @@
+import { AppContext } from "site/apps/site.ts";
+import { getRecordsByPath } from "apps/blog/utils/records.ts";
+import { Category } from "apps/blog/types.ts";
+
+const COLLECTION_PATH = "collections/blog/categories";
+const ACCESSOR = "category";
+
+export default async function BlogPostCategory(
+    _req: Request,
+    ctx: AppContext,
+): Promise<Category[] | null> {
+    const categorys = await getRecordsByPath<Category>(
+        ctx,
+        COLLECTION_PATH,
+        ACCESSOR,
+    );
+
+    return categorys;
+
+}
