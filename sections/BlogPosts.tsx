c1d98f7c14a33957e6a216dc17cfd47d0fe13cdb
ApMatheus <matheusaparecido101@gmail.com>
2024-06-22 17:54:01 -0300

    feat: BlogPostlayout

--- sections/BlogPosts.tsx
+++ sections/BlogPosts.tsx
@@ -212,6 +212,14 @@ export default function BlogPosts({
       pagination: { perPage, page: page + 1 },
     },
   })["f-partial"];
+  // Get the HTMX link for this section
+  const prevLink = usePartialSection({
+    mode: "append",
+    // Renders this section with the next page
+    props: {
+      pagination: { perPage, page: page - 1 },
+    },
+  })["f-partial"];
 
   function calculateReadingTime(words: number): string {
     const wordsPerMinute = 250;
@@ -223,40 +231,42 @@ export default function BlogPosts({
 
   const ContainerComponent = page === 0 ? Container : Fragment;
 
+  const numberPage = posts && posts?.length / perPage || 0;
+  const arrayPage = Array.from({ length: numberPage }, (_, i) => i + 1)
+
   return (
-    <ContainerComponent>
-      <>
-        <div class="gap-8 grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2">
-          {posts.slice(from, to).map((post) => (
+    <div class="w-full h-full text-primary-content bg-neutral" id={postList}>
+      <div class="flex flex-col lg:flex-row w-full mx-auto max-w-[1320px]">
+        <div class="gap-8 flex flex-col px-4 md:grid-cols-2 py-32 font-thicccboi  bg-neutral w-full" >
+          {posts.slice(from, to).map((post, index) => (
             <a
               href={`/blog/${post.slug}`}
-              class="border border-secondary overflow-hidden rounded-lg"
+              class={`"overflow-hidden border border-base-200 rounded-lg bg-base-100 flex flex-col md:gap-6 ${index % 2 ? "md:flex-row" : "md:flex-row-reverse"}`}
             >
               <Image
-                width={380}
-                height={274}
-                class="object-fit w-full"
+                width={330}
+                height={186}
+                class="object-fit w-full md:w-2/4"
                 sizes="(max-width: 640px) 100vw, 30vw"
                 src={post.image || ""}
                 alt={post.image}
                 decoding="async"
                 loading="lazy"
               />
-              <div class="p-6 space-y-4">
-                <div class="font-semibold">
+              <div class={`p-3 md:p-5 space-y-4 md:w-2/4 relative md:before:content-[''] md:before:w-16 md:before:h-16 md:before:bg-base-100 md:before:absolute md:before:top-7  ${index % 2 ? "md:before:-left-14" : "md:before:-right-14"} md:before:rotate-45 md:before:z-[-1] z-10`}>
+                <div class="font-semibold text-secondary">
                   {calculateReadingTime(post.content.split(" ").length)}
                 </div>
-                <div class="space-y-2">
-                  <h3 class="text-2xl">{post.title}</h3>
-                  <p class="text-base">{post.excerpt}</p>
-                </div>
+
+                <h3 class=" text-2xl font-bold">{post.title}</h3>
                 <div class="flex flex-wrap gap-2">
                   {post.categories?.map((category) => (
-                    <div class="badge badge-lg badge-primary text-xs">
+                    <div class="badge badge-lg badge-primary text-secondary text-xs">
                       {category.name}
                     </div>
                   ))}
                 </div>
+                <p class="text-base">{post.excerpt}</p>
                 <div class="flex flex-wrap gap-2">
                   <span>
                     {post.date
@@ -271,26 +281,86 @@ export default function BlogPosts({
                   <span>{post.authors[0]?.name}</span>
                 </div>
               </div>
-            </a>
-          ))}
+            </a >
+          ))
+          }
+          {
+            arrayPage.length > 0 && (
+              <div class="flex flex-row gap-4">
+                <div class="flex justify-center w-full" >
+                  <button
+                    hx-get={prevLink}
+                    hx-swap="outerHTML"
+                    hx-target={`#${postList}`}
+                    aria-label={cta.text}
+                    class="btn px-2 btn-secondary disabled:bg-secondary disabled:opacity-50"
+                    disabled={page == 0}
+                  >
+                    <span class="inline [.htmx-request_&]:hidden">
+                      <span class="flex transition rotate-90 ">
+                        <svg
+                          width="32"
+                          height="33"
+                          viewBox="0 0 32 33"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M8.17674 12.5577L8.17676 12.5577L8.5303 12.2041C8.53031 12.2041 8.53031 12.2041 8.53032 12.2041C8.62794 12.1065 8.78621 12.1065 8.88385 12.2041C8.88385 12.2041 8.88385 12.2041 8.88385 12.2041L15.6464 18.9667L16 19.3202L16.3535 18.9667L23.1161 12.2041C23.2138 12.1064 23.372 12.1064 23.4696 12.2041L23.8232 12.5577C23.9208 12.6553 23.9208 12.8135 23.8232 12.9112L16.1767 20.5577C16.0791 20.6553 15.9209 20.6553 15.8232 20.5577L8.17674 12.9112C8.17674 12.9112 8.17674 12.9112 8.17674 12.9112C8.07911 12.8135 8.07911 12.6553 8.17674 12.5577Z"
+                            fill="#fff"
+                            stroke="#fff"
+                          />
+                        </svg>
+                      </span>
+                    </span>
+                    <span class="loading loading-spinner hidden [.htmx-request_&]:block" />
+                  </button>
+                </div>
+                <ul class="flex flex-row gap-2 justify-center items-center">
+                  {arrayPage.map((index) =>
+                    <li class={`${index == page + 1 ? "bg-transparent" : "bg-secondary"} font-bold text-lg h-max p-1 text-primary`}>
+                      {index}
+                    </li>)}
+                </ul>
+                <div class="flex justify-center w-full" >
+                  <button
+                    hx-get={fetchMoreLink}
+                    hx-swap="outerHTML"
+                    hx-target={`#${postList}`}
+                    aria-label={cta.text}
+                    class="btn px-2 btn-secondary disabled:bg-secondary disabled:opacity-50"
+                    disabled={page == (arrayPage.length - 1)}
+                  >
+                    <span class="inline [.htmx-request_&]:hidden">
+                      <span class="flex transition -rotate-90 ">
+                        <svg
+                          width="32"
+                          height="33"
+                          viewBox="0 0 32 33"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M8.17674 12.5577L8.17676 12.5577L8.5303 12.2041C8.53031 12.2041 8.53031 12.2041 8.53032 12.2041C8.62794 12.1065 8.78621 12.1065 8.88385 12.2041C8.88385 12.2041 8.88385 12.2041 8.88385 12.2041L15.6464 18.9667L16 19.3202L16.3535 18.9667L23.1161 12.2041C23.2138 12.1064 23.372 12.1064 23.4696 12.2041L23.8232 12.5577C23.9208 12.6553 23.9208 12.8135 23.8232 12.9112L16.1767 20.5577C16.0791 20.6553 15.9209 20.6553 15.8232 20.5577L8.17674 12.9112C8.17674 12.9112 8.17674 12.9112 8.17674 12.9112C8.07911 12.8135 8.07911 12.6553 8.17674 12.5577Z"
+                            fill="#fff"
+                            stroke="#fff"
+                          />
+                        </svg>
+                      </span>
+                    </span>
+                    <span class="loading loading-spinner hidden [.htmx-request_&]:block" />
+                  </button>
+                </div>
+              </div >
+            )
+          }
+        </div >
+        <div class="w-full lg:w-80 px-2 h-screen bg-secondary">
+
         </div>
-        {to < posts.length && (
-          <div class="flex justify-center w-full" id={postList}>
-            <button
-              hx-get={fetchMoreLink}
-              hx-swap="outerHTML"
-              hx-target={`#${postList}`}
-              aria-label={cta.text}
-              class="btn btn-primary"
-            >
-              <span class="inline [.htmx-request_&]:hidden">
-                {cta.text}
-              </span>
-              <span class="loading loading-spinner hidden [.htmx-request_&]:block" />
-            </button>
-          </div>
-        )}
-      </>
-    </ContainerComponent>
+
+      </div>
+    </div>
+
   );
 }
