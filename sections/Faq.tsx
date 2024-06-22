baff3d0c977cf251804272d8d8bd10dffe5841a4
ApMatheus <matheusaparecido101@gmail.com>
2024-06-22 14:05:30 -0300

    feat: Form

--- sections/Faq.tsx
+++ sections/Faq.tsx
@@ -24,7 +24,7 @@ const _DEFAULT_IMAGE =
 export default function BlogPosts({
   title = "FAQs",
   description =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.",
   cta = { id: "change-me", href: "/", text: "Change me", outline: true },
   questions = [
     {
@@ -54,58 +54,93 @@ export default function BlogPosts({
     },
   ],
 }: Props) {
+
+  const media = Math.round(questions.length / 2)
+
   return (
-    <div class="container lg:mx-auto mx-4 text-sm py-12 lg:py-28">
-      <div class="flex flex-col lg:flex-row gap-10 lg:gap-20 justify-between">
+    <div class="w-full h-full bg-neutral py-14">
+      <div class="flex flex-col gap-10 lg:gap-20 justify-center items-center mx-auto max-w-[1320px] px-4 md:px-14 lg:px-4 font-thicccboi">
         <div class="flex-none space-y-6 lg:w-2/5">
-          <p class="text-4xl leading-snug">
+          <p class="text-2xl mb-5 font-bold lg:text-[28px] md:mb-7 text-primary-content text-center ">
             {title}
           </p>
-          <p class="text-lg">
+          <p class="mb-8 text-[19px] text-center text-base-content font-medium">
             {description}
           </p>
-          <a
-            key={cta?.id}
-            id={cta?.id}
-            href={cta?.href}
-            target={cta?.href.includes("http") ? "_blank" : "_self"}
-            class={`font-normal btn btn-primary ${
-              cta.outline && "btn-outline"
-            }`}
-          >
-            {cta?.text}
-          </a>
         </div>
-        <div class="flex-auto border-primary border-t">
-          {questions?.map((question) => (
-            <details class="border-primary border-b group">
-              <summary class="text-lg cursor-pointer py-6 flex ">
-                <span class="flex-auto">{question.title}</span>
-                <span class="flex-none transition group-open:rotate-180">
-                  <svg
-                    width="32"
-                    height="33"
-                    viewBox="0 0 32 33"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M8.17674 12.5577L8.17676 12.5577L8.5303 12.2041C8.53031 12.2041 8.53031 12.2041 8.53032 12.2041C8.62794 12.1065 8.78621 12.1065 8.88385 12.2041C8.88385 12.2041 8.88385 12.2041 8.88385 12.2041L15.6464 18.9667L16 19.3202L16.3535 18.9667L23.1161 12.2041C23.2138 12.1064 23.372 12.1064 23.4696 12.2041L23.8232 12.5577C23.9208 12.6553 23.9208 12.8135 23.8232 12.9112L16.1767 20.5577C16.0791 20.6553 15.9209 20.6553 15.8232 20.5577L8.17674 12.9112C8.17674 12.9112 8.17674 12.9112 8.17674 12.9112C8.07911 12.8135 8.07911 12.6553 8.17674 12.5577Z"
-                      fill="#18181B"
-                      stroke="#18181B"
-                    />
-                  </svg>
-                </span>
-              </summary>
-              <p
-                class="leading-relaxed mb-6 group-open:animate-fadeIn"
-                dangerouslySetInnerHTML={{ __html: question.answer }}
-              >
-              </p>
-            </details>
-          ))}
+        <div class="flex-auto w-full flex flex-col gap-4 lg:flex-row ">
+          <div class="w-full h-full flex flex-col gap-4 lg:w-2/4">
+            {questions?.map((question, index) => {
+              if (media > index)
+                return (
+                  <details class="rounded-lg group border border-base-200 w-full bg-base-100 p-4">
+                    <summary class="text-lg cursor-pointer flex ">
+                      <span class="flex-none transition group-open:rotate-180 ">
+                        <svg
+                          width="32"
+                          height="33"
+                          viewBox="0 0 32 33"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M8.17674 12.5577L8.17676 12.5577L8.5303 12.2041C8.53031 12.2041 8.53031 12.2041 8.53032 12.2041C8.62794 12.1065 8.78621 12.1065 8.88385 12.2041C8.88385 12.2041 8.88385 12.2041 8.88385 12.2041L15.6464 18.9667L16 19.3202L16.3535 18.9667L23.1161 12.2041C23.2138 12.1064 23.372 12.1064 23.4696 12.2041L23.8232 12.5577C23.9208 12.6553 23.9208 12.8135 23.8232 12.9112L16.1767 20.5577C16.0791 20.6553 15.9209 20.6553 15.8232 20.5577L8.17674 12.9112C8.17674 12.9112 8.17674 12.9112 8.17674 12.9112C8.07911 12.8135 8.07911 12.6553 8.17674 12.5577Z"
+                            fill="#ffcd00"
+                            stroke="#ffcd00"
+                          />
+                        </svg>
+                      </span>
+                      <span class="flex-auto font-bold group-hover:text-secondary group-open:text-secondary duration-100">{question.title}</span>
+                    </summary>
+                    <div class="pl-8 pt-5">
+                      <span
+                        class="leading-relaxed font-medium group-open:animate-fadeIn "
+                        dangerouslySetInnerHTML={{ __html: question.answer }}
+                      >
+                      </span>
+                    </div>
+                  </details>
+                )
+              else null
+            })}
+          </div>
+          <div class="w-full h-full flex flex-col gap-4 lg:w-2/4">
+            {questions?.map((question, index) => {
+              if (media <= index)
+                return (
+                  <details class="rounded-lg group border border-base-200 w-full bg-base-100 p-4">
+                    <summary class="text-lg cursor-pointer flex ">
+                      <span class="flex-none transition group-open:rotate-180 ">
+                        <svg
+                          width="32"
+                          height="33"
+                          viewBox="0 0 32 33"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M8.17674 12.5577L8.17676 12.5577L8.5303 12.2041C8.53031 12.2041 8.53031 12.2041 8.53032 12.2041C8.62794 12.1065 8.78621 12.1065 8.88385 12.2041C8.88385 12.2041 8.88385 12.2041 8.88385 12.2041L15.6464 18.9667L16 19.3202L16.3535 18.9667L23.1161 12.2041C23.2138 12.1064 23.372 12.1064 23.4696 12.2041L23.8232 12.5577C23.9208 12.6553 23.9208 12.8135 23.8232 12.9112L16.1767 20.5577C16.0791 20.6553 15.9209 20.6553 15.8232 20.5577L8.17674 12.9112C8.17674 12.9112 8.17674 12.9112 8.17674 12.9112C8.07911 12.8135 8.07911 12.6553 8.17674 12.5577Z"
+                            fill="#ffcd00"
+                            stroke="#ffcd00"
+                          />
+                        </svg>
+                      </span>
+                      <span class="flex-auto font-bold group-hover:text-secondary group-open:text-secondary duration-100">{question.title}</span>
+                    </summary>
+                    <div class="pl-8 pt-5">
+                      <span
+                        class="leading-relaxed font-medium group-open:animate-fadeIn "
+                        dangerouslySetInnerHTML={{ __html: question.answer }}
+                      >
+                      </span>
+                    </div>
+                  </details>
+                )
+              else null
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 }
