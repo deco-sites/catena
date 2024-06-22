1b3d09c1e1a3b854f57c78b6817753b72e013105
ApMatheus <matheusaparecido101@gmail.com>
2024-06-21 22:58:56 -0300

    fix: Sections Companies

--- sections/Companies.tsx
+++ sections/Companies.tsx
@@ -14,19 +14,19 @@ export interface Props {
 export default function Companies(props: Props) {
     const { title, icons } = props
     return (
-        <div class="flex w-full h-full">
-            <div class="w-full max-w-[1320px] mx-auto px-4">
+        <div class="flex w-full h-full py-8">
+            <div class="w-full max-w-[1320px] mx-auto px-4 text-center">
                 <h2 class="text-2xl mb-5 font-bold lg:text-[28px] md:mb-7">
                     {title}
                 </h2>
-                <ul class="flex flex-row flex-wrap gap-2">
+                <ul class="flex flex-row flex-wrap gap-2 w-full">
                     {icons.map((icon) =>
-                        <li class={"w-2/4"}>
-                            <Image src={icon.img} alt={icon.alt} width={100} height={100} class={"object-cover"} />
+                        <li class={"w-[calc(50%-0.5rem)] flex justify-center items-center md:w-[calc(33.333%-0.5rem)] lg:w-[calc(16.666%-0.5rem)]"} >
+                            < Image src={icon.img} alt={icon.alt} width={130} height={130} class={"object-cover"} />
                         </li>
                     )}
                 </ul>
             </div>
-        </div>
+        </div >
     )
 }
