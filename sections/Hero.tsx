ae96b19657e2ba0c6a18632f89a36b9d8502127f
ApMatheus <matheusaparecido101@gmail.com>
2024-06-21 21:51:47 -0300

    fix: hero desktop

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -32,14 +32,14 @@ export default function Hero(
 
   return (
     <div class="w-full bg-primary h-full mt-24 mb-9 after:border-l-[100vw] after:border-l-primary relative after:border-b-[4rem] after:-bottom-[3.995rem] after:border-transparent after:absolute after:bg-transparent after:-z-10 after:left-0 after:right-0">
-      <div class="lg:container flex flex-col lg:flex-row px-3 pt-6 ">
-        <div class="flex flex-col text-base-100 font-thicccboi gap-5">
-          <h2 class="font-bold text-xl">
+      <div class=" max-w-[1320px] container flex flex-col lg:flex-row px-3 pt-6 lg:pt-14 lg:pb-8 ">
+        <div class="flex flex-col text-base-100 font-thicccboi gap-5 lg:w-2/4">
+          <h2 class="font-bold text-xl lg:text-[38px] lg:leading-[40px] lg:mb-6">
             {title}
             <span class=" after-bg-custom relative ">{titleUnderline}</span>
           </h2>
           <span
-            class="text-base mb-7 font-medium"
+            class="text-base mb-7 font-medium lg:text-[17px]"
             dangerouslySetInnerHTML={{ __html: subTitle }}
           >
           </span>
@@ -56,16 +56,18 @@ export default function Hero(
             class=" bg-secondary text-primary w-max"
           />
         </div>
-        <Image
-          src={image}
-          alt={alt}
-          width={345}
-          height={380}
-          preload={preload}
-          fetchPriority={preload ? "high" : "low"}
-          loading={preload ? "eager" : "lazy"}
-          class=" -mb-32 mt-8"
-        />
+        <div class="w-full h-full flex justify-center items-center lg:w-2/4">
+          <Image
+            src={image}
+            alt={alt}
+            width={345}
+            height={380}
+            preload={preload}
+            fetchPriority={preload ? "high" : "low"}
+            loading={preload ? "eager" : "lazy"}
+            class=" -mb-32 mt-8 w-full h-auto max-w-[537px]"
+          />
+        </div>
       </div>
     </div>
   );
